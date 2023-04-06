f4b8ab4d35fe96d458bb10d4dd4d949c6a3fcaef
TracopkWb <tracopk.wb@gmail.com>
2023-04-05 20:17:55 -0600

    Deleting the cosole.logs
    Correct

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const inputPhoneNumber = document.getElementById("phoneNumber");
 
-inputPhoneNumber.addEventListener("keyup", getInput);
+inputPhoneNumber.addEventListener("input", getInput);
 
 function getInput(e) {
     e.preventDefault;
@@ -11,25 +11,37 @@ function getInput(e) {
         var formattedInputValue = formatPhoneNumber(inputPhoneNumber.value);
     };
     
-  };
+};
 
-  function formatPhoneNumber(value){
+function formatPhoneNumber(value){
     if(!value) return value;
+     log.textContent += ` ${value}`;
+};
 
-  };
+//   Script to show a preview of the chosen picture
+
+var addPictureBtn = document.getElementById("add-pic");
+
+addPictureBtn.addEventListener("click",addPic);
+
+function addPic(){
+    // console.log(1)
+    var picInput = document.getElementById("pic-input");
+    picInput.click();
+};
 
 
-  function showFile() {
+function showFile() {
     var file = document.getElementById("pic-input").files[0];
-    console.log(file)
-    console.log(file.name)
+    // console.log(file)
+    // console.log(file.name)
     var image = document.getElementById("dogPic"); 
     var reader = new FileReader();
 
     reader.addEventListener("load", ()=>{
         image.src=reader.result;
-        console.log(reader.result)
-        console.log(image);      
+        // console.log(reader.result)
+        // console.log(image);      
     });
     if(file){
         reader.readAsDataURL(file);
